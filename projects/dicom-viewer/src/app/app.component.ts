70e4a7596a8b1046b80fae0471374b0f55e50def
Cryt1c <david@peherstorfer.info>
2025-01-29 21:31:54 +0100

    Implement Angular event handling

--- projects/dicom-viewer/src/app/app.component.ts
+++ projects/dicom-viewer/src/app/app.component.ts
@@ -10,48 +10,52 @@ import { DicomViewer, initDicomViewerRs, setConsoleErrorPanicHook } from '../pub
 })
 export class AppComponent {
   title = 'dicom-viewer';
+  dicomViewer: DicomViewer | null = null;
+
   async ngOnInit() {
     await initDicomViewerRs();
     setConsoleErrorPanicHook();
-    const dicomViewer = DicomViewer.new();
-    const inputElement = document.getElementById("file-input");
-    const handleFiles = async (event: Event): Promise<void> => {
-      console.log(handleFiles);
-      const target = event.target as HTMLInputElement;
-      const files = Array.from(target.files || []);
-      const filesPromise = Array.from(files).map((file: Blob) => {
-        const fileReader = new FileReader();
-        return new Promise<Uint8Array>((resolve, reject) => {
-          fileReader.onload = () => {
-            if (fileReader.result instanceof ArrayBuffer) {
-              resolve(new Uint8Array(fileReader.result));
-            }
-            else {
-              reject(new Error("Failed to read file as Arraybuffer"));
-            }
-          }
-          fileReader.onerror = () => {
-            reject(fileReader.error);
-          }
-          fileReader.readAsArrayBuffer(file);
-        });
-      });
-      const loadedFiles = await Promise.all(filesPromise);
-      console.log(loadedFiles);
-      dicomViewer.read_files(loadedFiles);
-      dicomViewer.render_file_at_index(0);
-    }
-    inputElement?.addEventListener("change", handleFiles, false);
+    this.dicomViewer = DicomViewer.new();
+  }
 
-    const canvas = document.getElementById("viewer-canvas");
-    canvas?.addEventListener("wheel", (event) => {
-      if (event.deltaY < 0) {
-        console.log("next");
-        dicomViewer.render_next_file();
-      } else {
-        console.log("previous");
-        dicomViewer.render_previous_file();
-      }
+  async handleWheel(event: WheelEvent): Promise<void> {
+    if (!this.dicomViewer) {
+      return
+    }
+    if (event.deltaY < 0) {
+      console.log("next");
+      this.dicomViewer.render_next_file();
+    } else {
+      console.log("previous");
+      this.dicomViewer.render_previous_file();
+    }
+  }
+  async handleFiles(event: Event): Promise<void> {
+    if (!this.dicomViewer) {
+      return
+    }
+    const target = event.target as HTMLInputElement;
+    const files = Array.from(target.files || []);
+    const filesPromise = Array.from(files).map((file: Blob) => {
+      const fileReader = new FileReader();
+      return new Promise<Uint8Array>((resolve, reject) => {
+        fileReader.onload = () => {
+          if (fileReader.result instanceof ArrayBuffer) {
+            resolve(new Uint8Array(fileReader.result));
+          }
+          else {
+            reject(new Error("Failed to read file as Arraybuffer"));
+          }
+        }
+        fileReader.onerror = () => {
+          reject(fileReader.error);
+        }
+        fileReader.readAsArrayBuffer(file);
+      });
     });
+    const loadedFiles = await Promise.all(filesPromise);
+    console.log(loadedFiles);
+    this.dicomViewer.read_files(loadedFiles);
+    this.dicomViewer.render_file_at_index(0);
   }
 }
