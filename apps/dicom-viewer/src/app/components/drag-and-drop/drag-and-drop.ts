816aba8ef34bf2eea2f4ab19f460195b2404baa7
Cryt1c <david@peherstorfer.info>
2025-07-27 10:35:58 +0200

    Add drag-and-drop component

---
+++ apps/dicom-viewer/src/app/components/drag-and-drop/drag-and-drop.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-drag-and-drop',
+  imports: [CommonModule],
+  templateUrl: './drag-and-drop.html',
+  styleUrl: './drag-and-drop.css',
+})
+export class DragAndDropComponent {}
